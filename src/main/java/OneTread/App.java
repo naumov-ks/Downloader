1ae1e6c3db5cf8fba485bf466fa1ee712e977d9f
Konstantin Naumov <naumov.ks@gmail.com>
2018-02-16 10:52:43 +1000

    Сделал listFile

--- src/main/java/OneTread/App.java
+++ src/main/java/OneTread/App.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.google.common.util.concurrent.RateLimiter;
@@ -13,6 +14,7 @@ public class App {
 
 	public static void main(String[] args) throws Exception {
 		DGListFromFile text=new DGListFromFile("text.txt");
-		text.getListFiles();
+		ArrayList<GoalDownload> listFiles=text.getListFiles();
+		
 	}
 }
--- src/main/java/OneTread/DGListFromFile.java
+++ src/main/java/OneTread/DGListFromFile.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class DGListFromFile implements getGoalListFiles {
-private String pathFile; 
+private String pathFile;
+private ArrayList<GoalDownload> listFiles=new ArrayList<GoalDownload>();
 	
 DGListFromFile(String pathFile){
 	this.pathFile=pathFile;
@@ -19,7 +20,9 @@ public ArrayList<GoalDownload> getListFiles(){
 try(BufferedReader br=new BufferedReader(new FileReader(pathFile)))	{
 String text;	
 while((text=br.readLine())!=null){
-System.out.println(text);	
+String[] goalString=text.replace("\n", "").split(" ");
+listFiles.add(new GoalDownload(goalString[0], goalString[1]));
+
 }
 } catch (FileNotFoundException e) {
 	// TODO Auto-generated catch block
@@ -28,7 +31,7 @@ System.out.println(text);
 	// TODO Auto-generated catch block
 	e.printStackTrace();
 }
-return null;
+return listFiles;
 }
 	
 
